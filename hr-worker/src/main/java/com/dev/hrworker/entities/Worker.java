bb3dd5993117ad1c0c035cb235986c597f6c424a
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-11-22 10:00:57 -0300

    Implementando MS de hr-worker

---
+++ hr-worker/src/main/java/com/dev/hrworker/entities/Worker.java
@@ -0,0 +1,23 @@
+package com.dev.hrworker.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_worker")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Worker implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+}
