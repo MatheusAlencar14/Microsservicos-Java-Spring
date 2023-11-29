8c011511560f5175babfccacc4e361e1b273d778
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-11-29 11:29:34 -0300

    Criando projeto hr-config-server

---
+++ hr-config-server/src/main/java/com/dev/hrconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.dev.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigServerApplication.class, args);
+
+		System.out.printf("Deu certo!");
+	}
+
+}
