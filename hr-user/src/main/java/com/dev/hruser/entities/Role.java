3516eacc284aa1b7a21f5de1cba82acb939e8d7b
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-12-06 11:22:10 -0300

    Criando e associando Entidades User e Role

---
+++ hr-user/src/main/java/com/dev/hruser/entities/Role.java
@@ -0,0 +1,22 @@
+package com.dev.hruser.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_role")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roleName;
+}
